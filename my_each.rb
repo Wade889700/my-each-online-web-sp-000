aeba2a89489feb4917aa2982e3f4c759282d5db5
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 23:16:09 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,12 @@
 def my_each(array)
   my_array=[]
-  array.each do |word|
-    puts word
+  counter = 0
+  while counter < array.length
+    counter += 1
+    yield |word|
+    my_array.push(word)
   end
-
+  my_array
 end
+
+my_each{|word| array[counter]=word}

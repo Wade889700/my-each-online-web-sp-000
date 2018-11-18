3e1ab54f042f829f7e32fa74ffaf459cf8a5d685
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 23:21:21 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -6,16 +6,3 @@ def my_each(array)
     counter += 1
   end
 end
-
-my_each(array){|counter| my_array.push(array[counter])}
-
-def my_each(array)
-  i = 0
-  while i < array.length
-# The "while" method will always return a value of => nil"
-    yield(array[i])
-    i = i + 1
-  end
-end
-
-my_each(list) {|i| puts "So I think #{i} is a piece of shit"}

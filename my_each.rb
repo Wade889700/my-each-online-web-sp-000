34ca5e82119cb943c518ac6a5dad9c37ed7baebd
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 23:20:21 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -3,10 +3,10 @@ def my_each(array)
   counter = 0
   while counter < array.length
 
-    yield array[counter]
+    yield (array[counter])
     counter += 1
   end
   my_array
 end
 
-my_each(array){|i| my_array.push(array[i])}
+my_each(array){|counter| my_array.push(array[counter])}

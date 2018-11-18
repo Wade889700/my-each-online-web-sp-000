3813bb95dfcc1172fb438dddda9ada0071b478bf
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 23:24:24 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,9 @@
 def my_each(array)
   my_array=[]
-  counter = 0
+  i = 0
   while counter < array.length
     yield(array[counter])
-    counter += 1
+    i += 1
   end
 end
+my_each(array) {|i| puts i * 2}

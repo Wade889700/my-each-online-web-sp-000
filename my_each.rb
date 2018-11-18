5301f334f30531e4989d80ef50f33d9b5155c51e
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 23:38:44 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,9 @@
-
-
 def my_each(array)
-    a = 0
+    a = 0 #here you are setting a counter
 
-  while a < array.length
-    yield array[a]
-    a += 1
+  while a < array.length #and saying while the counter is less than the length of the array
+    yield array[a] #pass the element to the yielded block
+    a += 1 #increment the counter
   end
-  array
+  array #return the array 
 end
